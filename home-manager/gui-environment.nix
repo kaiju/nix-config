65eaa887c8d8aa2b57975dd3746f069570eb5e3d
Josh <josh@kaiju.net>
2022-02-20 22:17:56 -0500

    fix alacritty config

--- home-manager/gui-environment.nix
+++ home-manager/gui-environment.nix
@@ -129,12 +129,12 @@
         "TERM" = "xterm-256color";
       };
       window = {
+        opacity = 0.9;
         padding = {
           x = 10;
           y = 10;
         };
       };
-      #background_opacity = 0.9;
       font = {
         size = 12.0;
         normal = {
