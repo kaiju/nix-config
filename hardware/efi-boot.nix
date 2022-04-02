b6b492dba2fa7a4d45d8c27a413cf47d188a7fba
Josh <josh@kaiju.net>
2022-04-02 13:31:07 -0400

    have efi-boot actually efi boot

--- hardware/efi-boot.nix
+++ hardware/efi-boot.nix
@@ -9,6 +9,9 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   fileSystems."/" =
     { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
