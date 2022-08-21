68d1405c36c67035ffb998df981fc8a1bada0b4d
Josh <josh@mast.zone>
2022-08-21 19:45:06 -0400

    fix qemu guest config

--- hardware/qemu-guest.nix
+++ hardware/qemu-guest.nix
@@ -1,35 +1,32 @@
-{ nixpkgs, config, lib, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 {
   imports = [
-    "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+    "${toString modulesPath}/profiles/qemu-guest.nix"
   ];
 
-  config = {
-    fileSystems."/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-      autoResize = true;
-    };
-    
-    boot.growPartition = true;
-    boot.kernelParams = [ "console=ttyS0" ];
-    boot.loader.grub.device = "/dev/vda";
-    boot.loader.timeout = 0;
-
-    services.qemuGuest.enable = true;
-    systemd.services."serial-getty@ttyS0".enable = true;
-
-    system.build.rawimage = import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
-      inherit lib config pkgs;
-      format = "raw"
-    };
-
-    system.build.qcow2image = import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
-      inherit lib config pkgs;
-      format = "qcow2"
-    };
-
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+    autoResize = true;
   };
-}
+  
+  boot.growPartition = true;
+  boot.kernelParams = [ "console=ttyS0" ];
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.timeout = 0;
 
+  services.qemuGuest.enable = true;
+  systemd.services."serial-getty@ttyS0".enable = true;
+
+  system.build.rawimage = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    format = "raw";
+  };
+
+  system.build.qcow2image = import "${toString modulesPath}/../lib/make-disk-image.nix" {
+    inherit lib config pkgs;
+    format = "qcow2";
+  };
+
+}
 
