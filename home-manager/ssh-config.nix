e2f90e93de7ff8343e186e626bc7fd423b0fa7c5
Josh <josh@mast.zone>
2023-11-21 21:29:24 -0500

    specify identityfile for ssh config

--- home-manager/ssh-config.nix
+++ home-manager/ssh-config.nix
@@ -2,6 +2,9 @@
 {
   programs.ssh = {
     enable = true;
+    extraConfig = ''
+      IdentityFile ~/.ssh/josh@mast.zone.key
+    '';
     # wonder if we can just build this out of all our flake outputs?
     matchBlocks = {
       "mast.zone" = {
