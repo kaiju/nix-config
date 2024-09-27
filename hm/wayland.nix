5a0e25ab9d95b3de0b0a442e56ec9c8af0a5d60f
Josh <josh@mast.zone>
2024-09-26 21:38:44 -0400

    fix swayidle

--- hm/wayland.nix
+++ hm/wayland.nix
@@ -113,8 +113,8 @@
     timeouts = [
       {
         timeout = 300;
-        command = "swaymsg \"output * dpms off\"";
-        resumeCommand = "swaymsg \"output * dpms on\"";
+        command = "${pkgs.sway}/bin/swaymsg \"output * dpms off\"";
+        resumeCommand = "${pkgs.sway}/bin/swaymsg \"output * dpms on\"";
       }
     ];
   };
