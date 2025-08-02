1315abb7b1241ad48ef2109af83c7b5aecd5144a
Josh <josh@mast.zone>
2025-08-01 22:11:12 -0400

    tweak tabstop

--- hm/neovim.nix
+++ hm/neovim.nix
@@ -33,8 +33,8 @@
       vim.o.expandtab = true
       vim.o.number = true
       vim.o.termguicolors = true
-      vim.o.shiftwidth = 2
-      vim.o.tabstop = 2
+      vim.o.shiftwidth = 4 
+      vim.o.tabstop = 4 
       vim.o.smarttab = true
       vim.o.completeopt = "menu,menuone,noselect"
       vim.o.cursorline = true
