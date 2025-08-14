c3d6697a7aa5f67ef5adde9509c27c5308ebaf49
Josh <josh@mast.zone>
2025-08-14 16:27:55 -0400

    First stab at a nix-darwin config

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     agenix = {
       url = "github:ryantm/agenix";
@@ -19,221 +21,268 @@
 
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixos-hardware, nixos-wsl, home-manager, agenix }:
-  let
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nixos-hardware,
+      nixos-wsl,
+      nix-darwin,
+      home-manager,
+      agenix,
+    }:
+    let
 
-    nixosSystem = import lib/nixosSystem.nix { inherit nixpkgs home-manager; };
-    homeConfig = import lib/homeConfig.nix { inherit nixpkgs home-manager; };
-    vmImage = import lib/vmImage.nix { inherit nixpkgs self; };
+      nixosSystem = import lib/nixosSystem.nix { inherit nixpkgs home-manager; };
+      homeConfig = import lib/homeConfig.nix { inherit nixpkgs home-manager; };
+      vmImage = import lib/vmImage.nix { inherit nixpkgs self; };
 
-  in {
+    in
+    {
 
-    # Lenovo Thinkpad X1 (7th Gen)
-    nixosConfigurations.aether = nixosSystem {
-      host = "aether";
-      system = "x86_64-linux";
-      hardware = nixos/targets/thinkpad_x1.nix;
-      modules = [
-        nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
-        nixos/modules/workstation.nix
-        nixos/modules/laptop.nix
-        nixos/modules/user-josh.nix
-      ];
+      # Lenovo Thinkpad X1 (7th Gen)
+      nixosConfigurations.aether = nixosSystem {
+        host = "aether";
+        system = "x86_64-linux";
+        hardware = nixos/targets/thinkpad_x1.nix;
+        modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
+          nixos/modules/workstation.nix
+          nixos/modules/laptop.nix
+          nixos/modules/user-josh.nix
+        ];
+      };
+
+      # Desktop, WSL
+      nixosConfigurations.erebus = nixosSystem {
+        host = "erebus";
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            wsl = {
+              enable = true;
+              defaultUser = "josh";
+            };
+          }
+        ];
+      };
+
+      nixosConfigurations.straylight = nixosSystem {
+        host = "straylight";
+        system = "x86_64-linux";
+        hardware = nixos/targets/straylight.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      nixosConfigurations.x220 = nixosSystem {
+        host = "x220";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/thinkpad_x220.nix;
+        modules = [
+          nixos-hardware.nixosModules.lenovo-thinkpad-x220
+          ./nixos/modules/workstation.nix
+          ./nixos/modules/laptop.nix
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      # new linux workstation for futzing with ml
+      nixosConfigurations.arcimedes = nixosSystem {
+        host = "arcimedes";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/efi-boot.nix;
+        modules = [
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      # oracle cloud a1 vm
+      nixosConfigurations.armitage = nixosSystem {
+        host = "armitage";
+        system = "aarch64-linux";
+        hardware = ./nixos/targets/oci.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      # Cobbled together i7-3770 workstation in the garage
+      nixosConfigurations.garage = nixosSystem {
+        host = "garage";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/ugh.nix;
+      };
+
+      # sigint -- radio intelligence
+      nixosConfigurations.sigint = nixosSystem {
+        host = "sigint";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/sigint.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+        ];
+      };
+
+      # mess-around lab virtualisation machine, 16 core xeon, 192GB ram
+      nixosConfigurations.kronos = nixosSystem {
+        host = "kronos";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/efi-boot.nix;
+      };
+
+      # VMs
+
+      # shell host vm configuration
+      nixosConfigurations.shell = nixosSystem {
+        host = "shell";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/qemu-guest.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+        ];
+      };
+
+      nixosConfigurations.torrent = nixosSystem {
+        host = "torrent";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/qemu-guest.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      nixosConfigurations.ops = nixosSystem {
+        host = "ops";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/qemu-guest.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      # home k8s vm instance using k3s
+      nixosConfigurations.k8s = nixosSystem {
+        host = "k8s";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/qemu-guest.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+        ];
+      };
+
+      # testing/playground vm
+      nixosConfigurations.artemis = nixosSystem {
+        host = "artemis";
+        system = "x86_64-linux";
+        hardware = ./nixos/targets/qemu-guest.nix;
+        modules = [
+          ./nixos/modules/server.nix
+          ./nixos/modules/observability.nix
+          ./nixos/modules/user-josh.nix
+        ];
+      };
+
+      # Giving nix-darwin a shot
+      darwinConfigurations."joshs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.josh = {
+              home.sessionPath = [
+                "$HOME/.rd/bin"
+              ];
+              programs.ssh.matchBlocks."*".identityFile = [
+                "~/.ssh/josh@fulcradynamics.com.key"
+              ];
+              home.stateVersion = "25.11";
+              imports = [
+                hm/josh.nix
+                hm/work.nix
+                hm/darwin.nix
+                hm/dev-tools.nix
+              ];
+            };
+          }
+
+          {
+            nixpkgs.hostPlatform = "aarch64-darwin";
+            nixpkgs.overlays = import ./lib/overlays.nix;
+            system.stateVersion = 6;
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+            nix.settings.experimental-features = "nix-command flakes";
+            ids.gids.nixbld = 30000;
+            environment.systemPackages = with nixpkgs.legacyPackages.aarch64-darwin; [
+              vim
+            ];
+            users.users.josh.home = "/Users/josh";
+          }
+
+        ];
+
+      };
+      /*
+        Surprise! Turns out home-manager has flake support!
+
+        This is useful for providing my home configurations in non-NixOS contexts, like
+        my work laptop, WSL2, etc where previously I was setting up a separate ~/.config/nixpkgs/home.nix
+        file.
+      */
+      homeConfigurations.macbook = homeConfig {
+        system = "aarch64-darwin";
+        modules = [
+          {
+            home.homeDirectory = nixpkgs.lib.mkOverride 10 "/Users/josh";
+            home.sessionPath = [
+              "$HOME/.rd/bin"
+            ];
+            programs.ssh.matchBlocks."*".identityFile = [
+              "~/.ssh/josh@fulcradynamics.com.key"
+            ];
+          }
+          hm/work.nix
+          hm/darwin.nix
+          hm/dev-tools.nix
+        ];
+      };
+
+      homeConfigurations.wsl = homeConfig {
+        system = "x86_64-linux";
+        modules = [
+          hm/dev-tools.nix
+        ];
+      };
+
+      # EXPERIMENTAL STUFF
+      build-vm = vmImage {
+        nixosConfiguration = self.nixosConfigurations.ops;
+      };
+
+      devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+        packages = with nixpkgs.legacyPackages.x86_64-linux; [
+          python311
+          openssh
+          rage
+          jq
+        ];
+      };
+
     };
-
-    # Desktop, WSL
-    nixosConfigurations.erebus = nixosSystem {
-      host = "erebus";
-      system = "x86_64-linux";
-      modules = [
-        nixos-wsl.nixosModules.default
-	{
-	  wsl = {
-	    enable = true;
-	    defaultUser = "josh";
-	  };
-	}
-      ];
-    };
-
-    nixosConfigurations.straylight = nixosSystem {
-      host = "straylight";
-      system = "x86_64-linux";
-      hardware = nixos/targets/straylight.nix;
-      modules = [
-	./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-	./nixos/modules/user-josh.nix
-      ];
-    };
-
-    nixosConfigurations.x220 = nixosSystem {
-      host = "x220";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/thinkpad_x220.nix;
-      modules = [
-        nixos-hardware.nixosModules.lenovo-thinkpad-x220
-        ./nixos/modules/workstation.nix
-        ./nixos/modules/laptop.nix
-        ./nixos/modules/user-josh.nix
-      ];
-    };
-
-    # new linux workstation for futzing with ml
-    nixosConfigurations.arcimedes = nixosSystem {
-      host = "arcimedes";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/efi-boot.nix;
-      modules = [
-        ./nixos/modules/user-josh.nix
-      ];
-    };
-
-    # oracle cloud a1 vm
-    nixosConfigurations.armitage = nixosSystem {
-      host = "armitage";
-      system = "aarch64-linux";
-      hardware = ./nixos/targets/oci.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-        ./nixos/modules/user-josh.nix
-      ];
-    };
-
-    # Cobbled together i7-3770 workstation in the garage 
-    nixosConfigurations.garage = nixosSystem {
-      host = "garage";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/ugh.nix;
-    };
-
-    # sigint -- radio intelligence
-    nixosConfigurations.sigint = nixosSystem {
-      host = "sigint";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/sigint.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-      ];
-    };
-
-    # mess-around lab virtualisation machine, 16 core xeon, 192GB ram
-    nixosConfigurations.kronos = nixosSystem {
-      host = "kronos";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/efi-boot.nix;
-    };
-
-    /*
-      VMs
-    */
-
-    # shell host vm configuration
-    nixosConfigurations.shell = nixosSystem {
-      host = "shell";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/qemu-guest.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-      ];
-    };
-
-    nixosConfigurations.torrent = nixosSystem {
-      host = "torrent";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/qemu-guest.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-        ./nixos/modules/user-josh.nix
-      ];
-    };
-
-    nixosConfigurations.ops = nixosSystem {
-      host = "ops";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/qemu-guest.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-        ./nixos/modules/user-josh.nix
-      ];
-    };
-
-    # home k8s vm instance using k3s
-    nixosConfigurations.k8s = nixosSystem {
-      host = "k8s";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/qemu-guest.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-      ];
-    };
-
-    # testing/playground vm
-    nixosConfigurations.artemis = nixosSystem {
-      host = "artemis";
-      system = "x86_64-linux";
-      hardware = ./nixos/targets/qemu-guest.nix;
-      modules = [
-        ./nixos/modules/server.nix
-        ./nixos/modules/observability.nix
-        ./nixos/modules/user-josh.nix
-      ];
-    };
-
-    /*
-      Surprise! Turns out home-manager has flake support!
-
-      This is useful for providing my home configurations in non-NixOS contexts, like
-      my work laptop, WSL2, etc where previously I was setting up a separate ~/.config/nixpkgs/home.nix
-      file.
-    */
-    homeConfigurations.macbook = homeConfig {
-      system = "aarch64-darwin";
-      modules = [
-        {
-          home.homeDirectory = nixpkgs.lib.mkOverride 10 "/Users/josh";
-          home.sessionPath = [
-            "$HOME/.rd/bin"
-          ];
-          programs.ssh.matchBlocks."*".identityFile = [
-            "~/.ssh/josh@fulcradynamics.com.key"
-          ];
-        }
-        hm/work.nix
-        hm/darwin.nix
-        hm/dev-tools.nix
-      ];
-    };
-
-    homeConfigurations.wsl = homeConfig {
-      system = "x86_64-linux";
-      modules = [
-        hm/dev-tools.nix
-      ];
-    };
-
-    /*
-     * EXPERIMENTAL STUFF
-     */
-    build-vm = vmImage {
-      nixosConfiguration = self.nixosConfigurations.ops;
-    };
-
-    devShell.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      packages = with nixpkgs.legacyPackages.x86_64-linux; [
-        python311
-        openssh
-        rage
-        jq
-      ];
-    };
-
-  }; 
 }
-
