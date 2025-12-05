bdcc5f7a37d45b628bcbe5c1522a77af1f9b3c53
Josh <josh@mast.zone>
2025-12-04 23:32:06 -0500

    add nixd/nil to zed

--- hm/zed-editor.nix
+++ hm/zed-editor.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zed-editor = {
     enable = true;
     extensions = [ "nix" ];
     mutableUserSettings = true; # for now
+    extraPackages = with pkgs; [
+      nixd
+      nil
+    ];
   };
 }
