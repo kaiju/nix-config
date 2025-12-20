c2b6310b21e6613e39c3f13ae40454d8fce2fc7d
josh <josh@mast.zone>
2025-12-20 00:35:04 -0500

    add glib

--- nixos/modules/workstation.nix
+++ nixos/modules/workstation.nix
@@ -3,7 +3,7 @@
 
   Common configuration for workstations
 */
-{ ... }:
+{ pkgs, ... }:
 {
 
   # NTFS support
@@ -11,6 +11,9 @@
     "ntfs"
     "nfs"
   ];
+
+  # provides gio so we can mount things
+  environment.systemPackages = with pkgs; [ glib ];
 
   # allow us to build to aarch64
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
