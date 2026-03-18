09adc1b1d0e907979e64a725d98b8fbdb7748598
josh <josh@mast.zone>
2026-03-17 22:15:57 -0400

    Add sky public key

--- nixos/users/sky.nix
+++ nixos/users/sky.nix
@@ -1,10 +1,13 @@
-{ lib, config, pkgs, ... }:
+{ config, ... }:
 {
 
   users.users.sky = {
     uid = 1001;
     isNormalUser = true;
     extraGroups = [ "mast" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7CifyuD7p/qSi61K5MQ38ib6V1pUVFQh4eC2IrS++3 josh@aether"
+    ];
   };
 
   home-manager.users.sky = {
